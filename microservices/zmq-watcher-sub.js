cc65fdba47683c99dc9101621f2dc724afd2265d
Davide Savazzi <davide.savazzi@visma.com>
2018-03-19 10:58:48 +0000

    zeromq pub/sub

---
+++ microservices/zmq-watcher-sub.js
@@ -0,0 +1,15 @@
+'use strict';
+const zmq = require('zeromq');
+
+const subscriber = zmq.socket('sub');
+
+// subscribe to all messages
+subscriber.subscribe('');
+
+subscriber.on('message', data => {
+    const message = JSON.parse(data);
+    const date = new Date(message.timestamp);
+    console.log(`File "${message.file}" changed at ${date}`);
+});
+
+subscriber.connect("tcp://localhost:60400");
